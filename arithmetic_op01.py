816b879ee75266300d01cfff3d2bf97aea03f79d
Jasurbek-code <66709683+Jasurbek-code@users.noreply.github.com>
2021-12-21 19:24:38 +0500

    1-vazifa
    ikki o'zgaruvchini qo'shish

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -3,3 +3,9 @@
 # Create a variable 'num_two'   and assign it the value of the int 4.
 
 # Find the sum of  'num_one' and 'num_two' then assign to 'variable_total'
+num_one = 5   # birinchi o'zgaruvchi
+num_two = 4   # ikkinchi o'zgaruvchi
+variable_total = num_one + num_two    # o'zgaruvchilar yig'indisi
+
+# yig'indi toq bo'lsa 1, juft bo'lsa 0 chiqadi
+print(variable_total)
